2d04cdcd6e2f805ce46e03b9816be4fb8dcbf2d4
hrach <pierre.bidkhan@gmail.com>
2021-09-22 02:17:10 -0700

    added all

---
+++ words.py
@@ -0,0 +1,12 @@
+def print_upper(words):
+    for word in words:
+        print(word)
+        
+
+def must_start_with(words):
+    for word in words:
+        if word.startswith('e') or word.startswith('E'):
+            print(word.upper())
+
+
+
